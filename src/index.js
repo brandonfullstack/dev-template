9635dfdff93d5af8f1de45486bed2cd2f437ef4c
Brandon Wright <brandonfullstack@gmail.com>
2023-07-31 22:37:03 -0400

    Format index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   console.log("project Template");
 });
